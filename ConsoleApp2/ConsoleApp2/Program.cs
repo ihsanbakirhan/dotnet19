b1a39d281621c2cc0e05d508860f5bb571175281
iausem <semvs@ayvansaray.edu.tr>
2019-01-15 19:41:53 +0300

    powerball console uygulaması

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            powerball pw = new powerball();
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <=6 ; j++)
+                {
+                    Console.Write(pw.dene() + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            
+        }
+    }
+    class powerball
+    {
+        Random r = new Random();
+        
+        public int dene()
+        {
+            return r.Next(1, 56);
+        }
+    }
+}
