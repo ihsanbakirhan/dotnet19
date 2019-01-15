8c1285dc4c74fda4992305efb0fbaf40d8ebe2fe
iausem <semvs@ayvansaray.edu.tr>
2019-01-15 20:39:24 +0300

    kayan yazi

---
+++ kayan yazı/kayan yazı/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+namespace kayan_yazı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yazi = Console.ReadLine();
+            while (true)
+            {
+                Console.Clear();
+                string bosluklar = "";
+                for (int i = 0; i < Console.WindowWidth-yazi.Length; i++)
+                {
+                    bosluklar += " ";
+                
+                }
+                yazi = yazi.Substring(1) + bosluklar + yazi[0];
+                Console.Write(yazi);
+                Thread.Sleep(100);
+
+            }
+        }
+    }
+}
